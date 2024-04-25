52d733db3b9c97a2ec2007871289f265333d3d16
rband709 <119666248+rband709@users.noreply.github.com>
2024-04-25 16:35:12 +0330

    Create insta.py

---
+++ plugins/Extra/insta.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+import os, requests, asyncio, math, time, wget
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from youtube_search import YoutubeSearch
+from youtubesearchpython import SearchVideos
+from yt_dlp import YoutubeDL
