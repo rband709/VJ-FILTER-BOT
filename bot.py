5f710f13352c03ef59906611deadd55f2bc3adde
rband709 <119666248+rband709@users.noreply.github.com>
2024-05-22 21:19:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,14 +25,14 @@ logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-# from database.ia_filterdb import Media
-# from database.users_chats_db import db
+from database.ia_filterdb import Media
+from database.users_chats_db import db
 from info import *
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from Script import script 
-# from datetime import date, datetime 
+from datetime import date, datetime 
 import pytz
 from aiohttp import web
 from plugins import web_server
